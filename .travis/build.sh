c68f424862f8faa044bca125e1cb0e2b563727dc
Markus Winter <markus@wntr.me>
2019-09-05 09:21:43 +0200

    Some travis with qemu tests

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# Host Machine
-HOST="" # Defaults to wherever the script is executed
-
-# Cross compile/testing action
 if [[ "$CROSS_COMPILE" == 1 ]]; then
 	if [[ "$ARM_ARCH_DIR" == "aarch64-linux-gnu" ]]; then
-		HOST="qemu-aarch64 -L /usr/aarch64-linux-gnu/";
+		#HOST="qemu-aarch64 -L /usr/aarch64-linux-gnu/";
+		echo "Cross compiling to aarch64"
 	else
-		HOST="qemu-arm -L /usr/arm-linux-gnueabi/";
+		#HOST="qemu-arm -L /usr/arm-linux-gnueabi/";
+		echo "Cross compiling to arm"
 	fi
+else # CROSS_COMPILE = false
+	swift build -c debug;
+	swift test -c debug;
+	swift build -c release;
 fi
-
-$HOST swift build -c debug;
-$HOST swift test -c debug;
-$HOST swift build -c release;
