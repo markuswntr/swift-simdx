cb59bd028e420dcbed1fd31bc0f51ad0c7d2a032
Markus Winter <markus@wntr.me>
2019-10-10 12:28:04 +0200

    Updates ios builds and adds watchos build

--- .travis/ios-build.sh
+++ .travis/ios-build.sh
@@ -10,13 +10,13 @@ xcodebuild -showsdks;
 swift package generate-xcodeproj;
 
 # iOS Build & Test
-xcodebuild -project SIMDX.xcodeproj -configuration Debug -scheme SIMDX-Package -destination "generic/platform=iOS" | xcpretty;
-xcodebuild test -project SIMDX.xcodeproj -configuration Debug -scheme SIMDX-Package -destination "OS=13.0,name=iPhone XS Max" | xcpretty;
+xcodebuild -project SIMDX.xcodeproj -configuration Debug -scheme SIMDX-Package -destination "generic/platform=iOS" | xcpretty; # Generic Device: No tests possible
+xcodebuild test -project SIMDX.xcodeproj -configuration Debug -scheme SIMDX-Package -destination "OS=13.0,name=iPad Pro (11-inch)" | xcpretty;
 
 # tvOS Build & Test
-xcodebuild -project SIMDX.xcodeproj -configuration Debug -scheme SIMDX-Package -destination "generic/platform=tvOS" | xcpretty;
+xcodebuild -project SIMDX.xcodeproj -configuration Debug -scheme SIMDX-Package -destination "generic/platform=tvOS" | xcpretty; # Generic Device: No tests possible
 xcodebuild test -project SIMDX.xcodeproj -configuration Debug -scheme SIMDX-Package -destination "OS=13.0,name=Apple TV 4K" | xcpretty;
 
 # watchOS Build & Test
-# xcodebuild -project SIMDX.xcodeproj -configuration Debug -scheme SIMDX-Package -destination "generic/platform=watchOS" | xcpretty; # Generic Device: No tests possible
-# xcodebuild -project SIMDX.xcodeproj -configuration Debug -scheme SIMDX-Package -destination "OS=6.0,name=Apple Watch Series 5 - 44mm" | xcpretty; # Apple Watch: No tests possible
+xcodebuild -project SIMDX.xcodeproj -configuration Debug -scheme SIMDX-Package -destination "generic/platform=watchOS" | xcpretty; # Generic Device: No tests possible
+xcodebuild -project SIMDX.xcodeproj -configuration Debug -scheme SIMDX-Package -destination "OS=6.0,name=Apple Watch Series 5 - 44mm" | xcpretty; # Apple Watch: No tests possible
